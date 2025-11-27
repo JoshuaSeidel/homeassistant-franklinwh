9d4a46da8fe9d2c1db227d7a7a1e6ab2187b6488
nrp929 <101362764+nrp929@users.noreply.github.com>
2025-11-26 17:45:46 -0800

    Update sensor.py
    Grid use change import to positive and export to negative.

--- sensor.py
+++ sensor.py
@@ -96,7 +96,7 @@ SENSOR_TYPES: tuple[FranklinWHSensorEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfPower.KILO_WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
-        value_fn=lambda data: data.stats.current.grid_use if data.stats else None,
+        value_fn=lambda data: -data.stats.current.grid_use if data.stats else None,
     ),
     FranklinWHSensorEntityDescription(
         key="grid_import",
@@ -136,7 +136,7 @@ SENSOR_TYPES: tuple[FranklinWHSensorEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfPower.KILO_WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
-        value_fn=lambda data: -data.stats.current.generator_production if data.stats else None,
+        value_fn=lambda data: data.stats.current.generator_production if data.stats else None,
     ),
     FranklinWHSensorEntityDescription(
         key="generator_energy",
